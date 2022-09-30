e352cc8659fd26bafb7f2b963ab886b4f0e72944
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-09-30 14:40:56 +0200

    Add function and state to handle button taps

--- App.js
+++ App.js
@@ -3,44 +3,50 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Row from './components/Row';
 
+
 export default function App() {
 
+  const [state, setState] = useState(initialState)
+
+  const HandleTap = (type, value) => {
+    setState(calculator(type, value, state));
+  };
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <SafeAreaView>
         <Text style={styles.value}>
-         Current value here
+          Current value
         </Text>
         <Row>
-          <Button title="C"></Button>
-          <Button title="+/-"></Button>
-          <Button title="%"></Button>
-          <Button title="÷"></Button>
+          <Button title="C" onPress={() => HandleTap("clear")}></Button>
+          <Button title="+/-" onPress={() => HandleTap("invert")}></Button>
+          <Button title="%" onPress={() => HandleTap("operator", "/")}></Button>
+          <Button title="÷" onPress={() => HandleTap("operator", "/")}></Button>
         </Row>
         <Row>
-          <Button title="7"></Button>
-          <Button title="8"></Button>
-          <Button title="9"></Button>
-          <Button title="x"></Button>
+          <Button title="7" onPress={() => HandleTap("number", 7)}></Button>
+          <Button title="8" onPress={() => HandleTap("number", 8)}></Button>
+          <Button title="9" onPress={() => HandleTap("number", 9)}></Button>
+          <Button title="x" onPress={() => HandleTap("operator", "*")}></Button>
         </Row>
         <Row>
-          <Button title="4"></Button>
-          <Button title="5"></Button>
-          <Button title="6"></Button>
-          <Button title="-"></Button>
+          <Button title="4" onPress={() => HandleTap("number", 4)}></Button>
+          <Button title="5" onPress={() => HandleTap("number", 5)}></Button>
+          <Button title="6" onPress={() => HandleTap("number", 6)}></Button>
+          <Button title="-" onPress={() => HandleTap("operator", "-")}></Button>
         </Row>
         <Row>
-          <Button title="1"></Button>
-          <Button title="2"></Button>
-          <Button title="3"></Button>
-          <Button title="+"></Button>
+          <Button title="1" onPress={() => HandleTap("number", 1)}></Button>
+          <Button title="2" onPress={() => HandleTap("number", 2)}></Button>
+          <Button title="3" onPress={() => HandleTap("number", 3)}></Button>
+          <Button title="+" onPress={() => HandleTap("operator", "+")}></Button>
         </Row>
         <Row>
-          <Button title="0"></Button>
-          <Button title=","></Button>
-          <Button title="="></Button>
+          <Button title="0" onPress={() => HandleTap("number", 0)}></Button>
+          <Button title="," onPress={() => HandleTap("separator", ".")}></Button>
+          <Button title="=" onPress={() => HandleTap("operator", "=")}></Button>
         </Row>
       </SafeAreaView>
 
