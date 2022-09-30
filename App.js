b6c1fa28ca0ef0ba47e5a9c09f195a7cba24c990
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-09-30 14:30:47 +0200

    Add buttons and rows to calculator. Add row component

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
+import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Row from './components/Row';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <SafeAreaView>
+        <Text style={styles.value}>
+         Current value here
+        </Text>
+        <Row>
+          <Button title="C"></Button>
+          <Button title="+/-"></Button>
+          <Button title="%"></Button>
+          <Button title="÷"></Button>
+        </Row>
+        <Row>
+          <Button title="7"></Button>
+          <Button title="8"></Button>
+          <Button title="9"></Button>
+          <Button title="x"></Button>
+        </Row>
+        <Row>
+          <Button title="4"></Button>
+          <Button title="5"></Button>
+          <Button title="6"></Button>
+          <Button title="-"></Button>
+        </Row>
+        <Row>
+          <Button title="1"></Button>
+          <Button title="2"></Button>
+          <Button title="3"></Button>
+          <Button title="+"></Button>
+        </Row>
+        <Row>
+          <Button title="0"></Button>
+          <Button title=","></Button>
+          <Button title="="></Button>
+        </Row>
+      </SafeAreaView>
+
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: 'skyblue',
+  },
+  value: {
+    color: "#FF5733",
+    fontSize: 42,
+    textAlign: "right",
+    marginBottom: 40,
   },
 });
