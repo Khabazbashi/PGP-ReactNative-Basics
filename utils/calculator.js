ca4126b02519de3a3e66128f0f43339e74783d35
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-09-30 14:46:02 +0200

    Implement functions handleoperator and handlenumber

--- utils/calculator.js
+++ utils/calculator.js
@@ -5,6 +5,73 @@ export const initialState = {
     startFresh: false
 }
 
+function handleNumber(value, state) {
+    if (state.startFresh) {
+        console.log("2")
+        return {
+            ...state,
+            currentValue: value,
+            startFresh: false
+        };
+    }
+    console.log("1")
+    return {
+        ...state,
+        currentValue: `${state.currentValue}${value}`
+    };
+}
+
+function handleOperator(value, state) {
+
+    if (state.previousValue === null) {
+        console.log("3")
+        return {
+            ...state,
+            previousValue: state.currentValue,
+            operator: value,
+            startFresh: true,
+        };
+    }
+
+    let result;
+    const current = parseFloat(state.currentValue);
+    const previous = parseFloat(state.previousValue);
+    switch (state.operator) {
+        case "+":
+            result = `${previous + current}`;
+            break;
+        case "-":
+            result = `${previous - current}`;
+            break;
+        case "*":
+            result = `${previous * current}`;
+            break;
+        case "/":
+            result = `${previous / current}`;
+            break;
+     }
+
+
+    if (value === "=") {
+        console.log("4")
+        console.log(state)
+        return {
+            ...initialState,
+            currentValue: result,
+            startFresh: true,
+        }
+    }
+
+    console.log("5")
+    return {
+        ...state,
+        currentValue: result,
+        previousValue: result, 
+        startFresh: true,
+        operator: value,
+    };
+};
+
 function calculator(type, value, state) {
     switch (type) {
         case "number":
@@ -15,7 +82,7 @@ function calculator(type, value, state) {
 
         case "clear":
                 return initialState
-
+        
         default:
             console.log("def")
             return state;
